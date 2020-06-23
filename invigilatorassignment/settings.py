1baff55ef98d5e72952be70965a2f1fe9be4a3e5
Vatsal56 <vjain@jpischool.com>
2020-06-23 14:59:09 +0530

    base

--- invigilatorassignment/settings.py
+++ invigilatorassignment/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'teachers.apps.TeachersConfig',
+    'assigner.apps.AssignerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'invigilatorassignment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
