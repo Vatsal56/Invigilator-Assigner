420dab89a0f1299847d5156076165079f5078f51
Vatsal56 <vjain@jpischool.com>
2020-06-23 23:15:04 +0530

    added and linked templates for assign, home and teachers

--- invigilatorassignment/settings.py
+++ invigilatorassignment/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'invigilatorassignment/static/')
     ]
 
--- invigilatorassignment/urls.py
+++ invigilatorassignment/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+import teachers.views
+import assigner.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name='home'),
+    path('teachers/', teachers.views.home, name='teachers_home'),
+    path('assigner/', assigner.views.home, name='assigner_home')
 ]
